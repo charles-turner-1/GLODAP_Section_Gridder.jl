ec10c02366796a18f3876638e9a8d40991654cf2
CTBot <charlesbot0987@gmail.com>
2026-04-27 22:12:25 +0800

    test: cover manual expocode_dir path

--- test/test_data_inspection.jl
+++ test/test_data_inspection.jl
@@ -6,4 +6,8 @@ using Test
     @test names(expocodes) == ["Year", "GLODAP Expocode", "GO-SHIP Expocode"]
     @test size(expocodes, 1) > 0
     @test expocodes[1, "GLODAP Expocode"] == "29HE19920714"
+
+    # Cover the manual expocode_dir branch as well as the default packaged-data path.
+    expocodes_from_dir = GSG.list_section_expocodes("A05", joinpath(GSG.root, "data", "SectionExpocodes"))
+    @test expocodes_from_dir == expocodes
 end
