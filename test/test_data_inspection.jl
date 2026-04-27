b2d2a843e4f3835c9ccd6942bf0fc20c6ca9d8ed
CTBot <charlesbot0987@gmail.com>
2026-04-27 21:35:21 +0800

    test: avoid DataFrames-specific nrow helper

--- test/test_data_inspection.jl
+++ test/test_data_inspection.jl
@@ -4,6 +4,6 @@ using Test
     expocodes = GSG.list_section_expocodes("A05")
 
     @test names(expocodes) == ["Year", "GLODAP Expocode", "GO-SHIP Expocode"]
-    @test nrow(expocodes) > 0
+    @test size(expocodes, 1) > 0
     @test expocodes[1, "GLODAP Expocode"] == "29HE19920714"
 end
